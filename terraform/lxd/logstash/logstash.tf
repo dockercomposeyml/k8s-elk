61b9861383513348d1cc9bdc6abd231d7428a76d
Shailesh Thakur <timime687@gmail.com>
2020-06-06 10:39:20 +0545

    [ Refactor ] [ Logstash/Kafka ] [ Summary ] # Refactored

--- terraform/lxd/logstash/logstash.tf
+++ terraform/lxd/logstash/logstash.tf
@@ -2,7 +2,7 @@ module "lxd-iaac"{
   source="git::https://github.com/sylesh687/osgroup-techies.git//modules/lxd/logstash_container?ref=master"
   cpu=1
   mem="256MB"
-  con_name="devops4u"
+  con_name="logstash4u"
   img_name="baseimage"
   
 
