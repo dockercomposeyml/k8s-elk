3e1d6db26c698a1b8e1cd1205dde394ce56ea4e9
Shailesh Thakur <timime687@gmail.com>
2020-06-06 14:14:43 +0545

    [ Fix ] [ Kafka ] [ Summary ] # Removed the Redundant code

--- terraform/lxd/kafka/kafka.tf
+++ terraform/lxd/kafka/kafka.tf
@@ -2,7 +2,7 @@ module "lxd-iaac"{
   source="git::https://github.com/sylesh687/osgroup-techies.git//modules/lxd/kafka_container?ref=master"
   cpu=1
   mem="256MB"
-  con_name="logstash4u"
+  con_name="kafka4u"
   img_name="baseimage"
   
 
